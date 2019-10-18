37fa0cc43363f3844b5772bc5dcf65a0153b85d4
Sergio3d <56047643+Sergio3d@users.noreply.github.com>
2019-10-18 14:48:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+     <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.scrollspy');
+            var instances = M.ScrollSpy.init(elems, options);
+         });
+        </script>
